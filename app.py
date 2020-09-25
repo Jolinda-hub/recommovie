a59620eaaf43491ccc1cb2bf0df15e28e69d1c66
Egemen Zeytinci <egemenzeytinci@gmail.com>
2020-09-25 15:18:21 +0300

    Fix minor bug about gunicorn

--- app.py
+++ app.py
@@ -7,8 +7,14 @@ from web.user import auth
 from util.elastic import Elastic
 from util import get_params
 
+# register blueprint
+app.register_blueprint(home, url_prefix='/')
+app.register_blueprint(rec_page, url_prefix='/recommendations')
+app.register_blueprint(info, url_prefix='/infocard')
+app.register_blueprint(auth, url_prefix='/user')
+app.register_blueprint(policy, url_prefix='/policy')
 
-def main():
+if __name__ == '__main__':
     elastic = Elastic()
     config = get_params()
 
@@ -16,18 +22,7 @@ def main():
     elastic.create_index()
     elastic.insert_elastic()
 
-    # register blueprint
-    app.register_blueprint(home, url_prefix='/')
-    app.register_blueprint(rec_page, url_prefix='/recommendations')
-    app.register_blueprint(info, url_prefix='/infocard')
-    app.register_blueprint(auth, url_prefix='/user')
-    app.register_blueprint(policy, url_prefix='/policy')
-
     # run web app
     port = config['app'].getint('port')
     debug = config['app'].getboolean('debug')
     app.run(host='0.0.0.0', port=port, debug=debug)
-
-
-if __name__ == '__main__':
-    main()
